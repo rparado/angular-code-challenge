4f762ba5b5aba1dfebaefef2a922f807829c185e
ROLAND PARADO <roland@stampede-design.com>
2024-04-08 15:19:42 +0800

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
-	{
-		path: 'user/:id' , 
-		component: UsersListComponent
-	},
+	
+	
 	{
 		path: '',
-		redirectTo: '',
+		redirectTo: '/user',
 		pathMatch: 'full'
-	}
+	},
+	{ path: 'user', component: UsersListComponent },
+	{
+		path: 'user/:id' , 
+		component: UserDetailComponent,
+		pathMatch: 'full'
+	},
 ];
 
 @NgModule({
